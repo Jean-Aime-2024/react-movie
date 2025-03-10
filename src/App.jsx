c8612ddd516eed23234c93aec182b7cfbde9685c
Jean-Aime-2024 <jeanaime1000@gmail.com>
2025-03-10 16:40:29 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,8 @@ const App = () => {
 
       const data = await response.json();
 
+      // console.log(data);
+
       if (data.Response === 'False') {
         setErrorMessage(data.Error || 'Failed to fetch movies');
         setMovieList([]);
